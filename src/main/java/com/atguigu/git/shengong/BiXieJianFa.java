40d2321e5ca782efa0107ac32c27816728bd78a5
zfc <879072741@qq.com>
2020-08-20 07:53:35 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/shengong/BiXieJianFa.java
@@ -0,0 +1,8 @@
+package com.atguigu.git.shengong;
+
+public class BiXieJianFa {
+    public static void main(String[] args) {
+        System.out.println("这是辟邪剑法第一式");
+        System.out.println("看剑");
+    }
+}
