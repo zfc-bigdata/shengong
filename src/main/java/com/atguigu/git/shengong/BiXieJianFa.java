fa687715263a02f6b0a00c130ea7c119a21d33ea
zfc <879072741@qq.com>
2020-08-20 08:05:26 +0800

    version2.0

--- src/main/java/com/atguigu/git/shengong/BiXieJianFa.java
+++ src/main/java/com/atguigu/git/shengong/BiXieJianFa.java
@@ -4,5 +4,11 @@ public class BiXieJianFa {
     public static void main(String[] args) {
         System.out.println("这是辟邪剑法第一式");
         System.out.println("看剑");
+
+    }
+    private static void guide() {
+        System.out.println("这是使用说明,爱看不看");
+        String first = "欲练此功, 必先自宫";
+        String second = "若不自宫, 也可练成";
     }
 }
