a2edb83624cfc438378d475939ad9e4246bbeff5
令狐冲 <792608181@qq.com>
2020-08-20 08:33:02 +0800

    添句话

--- src/main/java/com/atguigu/git/shengong/BiXieJianFa.java
+++ src/main/java/com/atguigu/git/shengong/BiXieJianFa.java
@@ -4,6 +4,7 @@ public class BiXieJianFa {
     public static void main(String[] args) {
         System.out.println("这是辟邪剑法第一式");
         System.out.println("看剑");
+        System.out.println("老东西这剑法有我一份功劳");
 
     }
     private static void guide() {
